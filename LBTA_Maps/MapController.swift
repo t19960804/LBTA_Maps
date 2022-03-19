902e4d826c58a835d2d364dc119a053e4ecad2d3
Unknown <t19960804@gmail.com>
2022-03-19 16:34:33 +0800

    Add Custom Annotations

--- LBTA_Maps/MapController.swift
+++ LBTA_Maps/MapController.swift
@@ -4,20 +4,49 @@ import LBTATools
 
 class MapController: UIViewController {
     let mapView = MKMapView()
+    let coordinate_SanFrancisco = CLLocationCoordinate2D(latitude: 37.7666, longitude: -122.427290)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(mapView)
-        mapView.fillSuperview()
+        setupUI()
         setupRegion()
+        setupAnnotations()
+    }
+    
+    func setupUI() {
+        view.addSubview(mapView)
+        mapView.delegate = self
+        mapView.fillSuperview()
     }
     
     func setupRegion() {
-        let center = CLLocationCoordinate2D(latitude: 25.036365, longitude: 121.453625)
         let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005) //值越小, 地圖可以顯示越精細
-        let region = MKCoordinateRegion(center: center, span: span)
+        let region = MKCoordinateRegion(center: coordinate_SanFrancisco, span: span)
         mapView.setRegion(region, animated: true)
     }
 
+    func setupAnnotations() {
+        let annotation1 = MKPointAnnotation()
+        annotation1.coordinate = coordinate_SanFrancisco
+        annotation1.title = "San Francisco"
+        annotation1.subtitle = "CA"
+        
+        let coordinate_AppleCampus = CLLocationCoordinate2D(latitude: 37.3326, longitude: -122.030024)
+        let annotation2 = MKPointAnnotation()
+        annotation2.coordinate = coordinate_AppleCampus
+        annotation2.title = "Apple Campus"
+        annotation2.subtitle = "CA"
+        
+        mapView.addAnnotations([annotation1, annotation2])
+        mapView.showAnnotations(mapView.annotations, animated: true)
+    }
 }
 
+extension MapController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "id")
+        view.canShowCallout = true
+        view.image = UIImage(named: "tourist")
+        return view
+    }
+}
