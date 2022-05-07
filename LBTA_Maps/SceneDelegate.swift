1d2d5a832085a10c80abcb48c2e023e39624ecaf
Unknown <t19960804@gmail.com>
2022-05-07 19:48:52 +0800

    Add LocalSearch In MapSearchingView

--- LBTA_Maps/SceneDelegate.swift
+++ LBTA_Maps/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -15,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = PlacesController()
+            window.rootViewController = UIHostingController(rootView: MapSearchingView())
 
             self.window = window
             window.makeKeyAndVisible()
--- LBTA_Maps/SwiftUIComponents/MapSearchingView.swift
+++ LBTA_Maps/SwiftUIComponents/MapSearchingView.swift
@@ -3,9 +3,11 @@ import MapKit
 
 // UIViewRepresentable > 將UIKit的View做包裝, 讓這個View可以放在SwiftUI裡面
 struct MapViewContainer: UIViewRepresentable {
+    var annotations = [MKPointAnnotation]()
+    
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
-        let coordinate = CLLocationCoordinate2D(latitude: 37.7666, longitude: -122.427290)
+        let coordinate = CLLocationCoordinate2D(latitude: 25.0475613, longitude: 121.5173399)
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         mapView.setRegion(region, animated: true)
@@ -13,19 +15,28 @@ struct MapViewContainer: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
-        
+        uiView.removeAnnotations(uiView.annotations)
+        if annotations.isEmpty == false {
+            uiView.addAnnotations(annotations)
+            uiView.showAnnotations(annotations, animated: true)
+        }
     }
     
     typealias UIViewType = MKMapView
 }
 struct MapSearchingView: View {
+    // 只要 @State變數 被改變, SwiftUI就會自動更新有使用到此變數的UI
+    // 若沒有這個機制, 很有可能變數更新了, 但是忘記更新UI
+    // 不然就是要額外的寫UI更新的function
+    @State var annotations = [MKPointAnnotation]()
+    
     var body: some View {
         ZStack(alignment: .top){ // 後面產生的元件將疊在之前的元件身上
-            MapViewContainer()
+            MapViewContainer(annotations: annotations)
                 .edgesIgnoringSafeArea(.all)
             HStack {
                 Button {
-                    print("123")
+                    performSearch(term: "Airports")
                 } label: {
                     Text("Search for airports")
                         .padding()
@@ -33,7 +44,7 @@ struct MapSearchingView: View {
                 }
                 
                 Button {
-                    
+                    annotations = []
                 } label: {
                     Text("Clear Annotations")
                         .padding()
@@ -41,6 +52,26 @@ struct MapSearchingView: View {
                 }
             }
             .shadow(radius: 3)
+        }
+    }
+    
+    private func performSearch(term: String) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = term
+        let localSearch = MKLocalSearch(request: request)
+        localSearch.start { response, error in
+            if let error = error {
+                print("Error - Find Nearby LandMark Fail:\(error)")
+                return
+            }
+            var airportAnnotations = [MKPointAnnotation]()
+            response?.mapItems.forEach {
+                let annotaion = MKPointAnnotation()
+                annotaion.title = $0.name
+                annotaion.coordinate = $0.placemark.coordinate
+                airportAnnotations.append(annotaion)
+            }
+            annotations = airportAnnotations
         }
     }
 }
