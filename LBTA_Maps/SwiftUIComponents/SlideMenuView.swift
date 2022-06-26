e487a1ad676bf7f7db5970e8a8fca0460c4b24ca
Unknown <t19960804@gmail.com>
2022-06-26 17:44:11 +0800

    Interactive Meun Items

--- LBTA_Maps/SwiftUIComponents/SlideMenuView.swift
+++ LBTA_Maps/SwiftUIComponents/SlideMenuView.swift
@@ -1,14 +1,23 @@
 import SwiftUI
 import MapKit
 
+struct MenuItem {
+    let id = UUID() // If you have custom types in your array, you should use id with whatever property inside your type identifies it uniquely.
+    let title: String
+    let imageName: String
+    let mapType: MKMapType
+}
+
 struct SlideMenuMapView: UIViewRepresentable {
+    var mapType: MKMapType!
+    
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
         return mapView
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
-        
+        uiView.mapType = self.mapType
     }
     
     typealias UIViewType = MKMapView
@@ -16,13 +25,17 @@ struct SlideMenuMapView: UIViewRepresentable {
 
 struct SlideMenuView: View {
     @State var isMenuShowing = false
+    @State var mapType = MKMapType.standard
+    let items = [MenuItem(title: "Standard", imageName: "car", mapType: .standard),
+                 MenuItem(title: "Hybrid", imageName: "antenna.radiowaves.left.and.right", mapType: .hybrid),
+                 MenuItem(title: "Globe", imageName: "safari", mapType: .satelliteFlyover)]
     
     var body: some View {
         GeometryReader { geometry in
             let width = geometry.size.width
             let height = geometry.size.height
             ZStack {
-                SlideMenuMapView()
+                SlideMenuMapView(mapType: self.mapType)
                     .edgesIgnoringSafeArea(.all)
                 VStack {
                     HStack {
@@ -32,7 +45,7 @@ struct SlideMenuView: View {
                             Image(systemName: "line.horizontal.3.decrease.circle.fill")
                                 .font(.system(size: 34 * getHScale()))
                                 .foregroundColor(Color.white)
-                                .shadow(radius: 5)
+                                .shadow(radius: 5 * getHScale())
                                 .padding()
                         }
                         Spacer()
@@ -49,7 +62,33 @@ struct SlideMenuView: View {
                         Color.white
                             .edgesIgnoringSafeArea(.all) //可以用Color當Background View
                         VStack {
-                            Text("Menu")
+                            HStack {
+                                Text("Menu")
+                                    .font(.system(size: 26 * getHScale(), weight: .bold))
+                                    .padding()
+                                Spacer()
+                            }
+                            HStack {
+                                VStack(alignment: .leading, spacing: 30 * getVScale()) {
+                                    ForEach(items, id: \.id) { item in
+                                        Button {
+                                            self.mapType = item.mapType
+                                            self.isMenuShowing.toggle()
+                                        } label: {
+                                            HStack {
+                                                Image(systemName: item.imageName)
+                                                    .foregroundColor(Color.black)
+                                                    .frame(width: 35 * getHScale(), height: 35 * getVScale())
+                                                Text("\(item.title)")
+                                                    .foregroundColor(Color.black)
+                                                    .font(.system(size: 20 * getHScale()))
+                                            }
+                                        }
+                                    }
+                                }
+                                Spacer()
+                            }
+                            .padding()
                             Spacer()
                         }
                     }
